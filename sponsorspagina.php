803a152d58dce431e54a4ea3c25f5ae68aad6862
Kevinxue12 <74967898+Kevinxue12@users.noreply.github.com>
2021-03-25 08:50:31 +0100

    css

---
+++ sponsorspagina.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content=""> 
+    <meta name="author" content="Kevin Xue">
+    <meta name="keywords" content=" ">
+    <title>Sponsors Pagina</title>
+    <link rel="stylesheet" type="text/css" href="css/sponsorstyle.css"> 
+
+</head>
+
+<body>
+    <header>
+        <section id="header1">
+            <a href="#"><img src="images/Logo_waifu_drinks_2.png" alt=""></a>
+            <nav>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Smaken</a></li>
+                <li><a href="#">Aanbiedingen</a></li>
+                <li><a href="#">Sponsors</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </nav>
+        </section>
+
+    </header>
+
+    <video autoplay muted loop>
+        <source src="images/5gQwb.daa.1.webm">
+    </video>
+
