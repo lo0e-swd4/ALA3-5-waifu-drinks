9e9003f09d07529b29c307d56165c8557359d411
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2021-04-07 14:32:19 +0200

    contact pagina 7-4

--- Contact/contact.js
+++ Contact/contact.js
@@ -2,8 +2,12 @@ window.onclick = function(element){
     var panel = element.target.nextElementSibling;
 
     switch(element.target.id){
-        case 'accpanel':
-            if(panel.style.height == "0px")
+        case 'accPanel1':
+            if(panel.style.height == "0px"){
+              panel.style.height = "200px";
+            }else {
+              panel.style.height = "0px";
+            }
         break;
     }
 }
